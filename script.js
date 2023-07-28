e3d70bb65267ade666ea2a2bd213d360455b6c88
SOHAM BASURI <128636304+Sohambasuri-2002@users.noreply.github.com>
2023-07-28 20:50:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const space = document.getElementById("space")
+const navlist = document.getElementById("navlist");
+
+space.addEventListener("click", ()=>{
+    navlist.classList.toggle("navlist-active")
+})
